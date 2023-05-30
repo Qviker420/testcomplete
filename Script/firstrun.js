e780b64f57039eb294e1adb74fd255a85e8154d7
user1 <chaligavaduta3@gmail.com>
2023-05-30 15:24:56 +0400

    TestCompleted

---
+++ Script/firstrun.js
@@ -0,0 +1,11 @@
+﻿function hello_world()
+{
+  Log.Message("Hello World");
+}
+
+function order_test()
+{
+  KeywordTests.open_order.Run();
+  KeywordTests.add_order.Run();
+  KeywordTests.close_app.Run();
+}
